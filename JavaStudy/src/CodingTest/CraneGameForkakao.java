adb66d0ff02b9421d005301726e1063b73ed8b06
Im-Gyo <jywlsh3@naver.com>
2020-04-16 20:29:47 +0900

    CraneGame CodingTest - 1

--- JavaStudy/src/CodingTest/CraneGameForkakao.java
+++ JavaStudy/src/CodingTest/CraneGameForkakao.java
@@ -4,22 +4,30 @@ package CodingTest;
 import java.util.Stack;
 
 public class CraneGameForkakao {
-	public int solution(int[][] board, int[] moves) {
+	public static void main(String[] args) {
+		int res = solution(null, null); 
+		System.out.println(res);
+	}
+	
+	public static int solution(int[][] board, int[] moves) {
+		Stack<Integer> stack = new Stack<Integer>();
 		board = new int[][] {{0,0,0,0,0},{1,2,5,2,0},{1,1,4,3,2},{5,5,2,1,3},{1,2,5,1,2}};
-		moves = new int[] {1,4,2,3,5}; 
+		System.out.println(board[1][0]);
+		moves = new int[] {1,4,2,3,5,2,1,4,3,3};
+		System.out.println(moves[0]);
 		int answer = 0;
 		
-		for(int i=0; i<5; i++) {
-			for(int j=0; j<5; j++) {
-				if(board[i][j] != 0) {
-					System.out.println(board[i][j]);			
-					board[i][j] = 0;
+		for (int j = 1; j <= moves.length; j++) {
+			int indexVal = moves[j - 1];
+			for (int i = 0; i < 5; i++) {
+				if (board[i][indexVal - 1] != 0) {
+					System.out.println(board[i][indexVal -1]);
+					stack.push(board[i][indexVal -1]);			
+					board[i][indexVal -1] = 0;
+					break;
 				}
 			}
-		}
-		
-		Stack<Integer> stack = new Stack<Integer>();
-		
+		}		
 		
 		return answer;
 	}
