58532d4ab16573264a163d50dbcef82c8b8a3e2d
Im-Gyo <jywlsh3@naver.com>
2020-04-23 23:56:58 +0900

    Temp

--- JavaStudy/src/CodingTest/CraneGameForkakao.java
+++ JavaStudy/src/CodingTest/CraneGameForkakao.java
@@ -6,30 +6,31 @@ import java.util.Stack;
 
 public class CraneGameForkakao {
 	public static void main(String[] args) {
-		int res = solution(null, null); 
-		System.out.println(res);
+		int res = solution(null, null);		
 	}
 	
 	public static int solution(int[][] board, int[] moves) {
 		Stack<Integer> stack = new Stack<Integer>();
-		board = new int[][] {{0,0,0,0,0},{1,2,5,2,0},{1,1,4,3,2},{5,5,2,1,3},{1,2,5,1,2}};
-		System.out.println(board[1][0]);
-		moves = new int[] {1,4,2,3,5,2,1,4,3,3};
-		System.out.println(moves[0]);
+		board = new int[][] {{0,0,0,0,0},{1,2,5,2,0},{1,1,4,3,2},{5,5,2,1,3},{1,2,5,1,2}}; 	// 필드정보
+		moves = new int[] {1,4,2,3,5,2,1,4,3,3};	// 크레인 움직임 정보(1줄 4줄 2줄 3줄)
 		int answer = 0;
-		
+		int peekIndex = 0;
+				
 		for (int j = 1; j <= moves.length; j++) {
-			int indexVal = moves[j - 1];
+			int indexVal = moves[j - 1];			
 			for (int i = 0; i < 5; i++) {
 				if (board[i][indexVal - 1] != 0) {
-					System.out.println(board[i][indexVal -1]);
-					stack.push(board[i][indexVal -1]);			
+					//System.out.println(board[i][indexVal -1]);
+					stack.push(board[i][indexVal -1]);
+					peekIndex = stack.search(stack.peek());
+					if((stack.empty() != false) )
+					System.out.println(stack.search(stack.peek()));
 					board[i][indexVal -1] = 0;
 					break;
 				}
 			}
 		}		
-		
+		System.out.println(stack.peek());
 		return answer;
 	}
 }
